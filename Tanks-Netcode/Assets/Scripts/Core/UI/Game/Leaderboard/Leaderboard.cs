ac69ad32b3e7be2edcaa39a767f89c1aa52e54f3
Marcelo Alves <48249122+MarceloAlves8799@users.noreply.github.com>
2023-09-05 19:30:24 -0300

    Leaderboard spawning
    18) Spawning and removing the leaderboard player's ranking

--- Tanks-Netcode/Assets/Scripts/Core/UI/Game/Leaderboard/Leaderboard.cs
+++ Tanks-Netcode/Assets/Scripts/Core/UI/Game/Leaderboard/Leaderboard.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -9,11 +12,125 @@ namespace Tanks
         [SerializeField] private LeaderboardEntityDisplay leaderboardEntityPrefab;
 
         private NetworkList<LeaderboardEntityState> leaderboardEntities;
+        private List<LeaderboardEntityDisplay> entityDisplays = new List<LeaderboardEntityDisplay>();
 
 
         private void Awake()
         {
             leaderboardEntities = new NetworkList<LeaderboardEntityState>();
         }
+
+        public override void OnNetworkSpawn()
+        {
+            if (IsClient)
+            {
+                leaderboardEntities.OnListChanged += HandleLeaderboardEntitiesChanged;
+                
+                foreach(LeaderboardEntityState entity in leaderboardEntities)
+                {
+                    HandleLeaderboardEntitiesChanged(new NetworkListEvent<LeaderboardEntityState>
+                    {
+                        Type = NetworkListEvent<LeaderboardEntityState>.EventType.Add,
+                        Value = entity
+                    });
+                }
+            }
+
+            if (IsServer)
+            {
+                TankPlayer[] players = FindObjectsOfType<TankPlayer>();
+                foreach (TankPlayer player in players)
+                {
+                    HandlePlayerSpawned(player);
+                }
+
+                TankPlayer.OnPlayerSpawned += HandlePlayerSpawned;
+                TankPlayer.OnPlayerDespawned += HandlePlayerDespawned;
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (IsClient)
+            {
+                leaderboardEntities.OnListChanged -= HandleLeaderboardEntitiesChanged;
+            }
+
+            if (IsServer)
+            {
+                TankPlayer.OnPlayerSpawned -= HandlePlayerSpawned;
+                TankPlayer.OnPlayerDespawned -= HandlePlayerDespawned;
+            }
+        }
+
+        private void HandleLeaderboardEntitiesChanged(NetworkListEvent<LeaderboardEntityState> changeEvent)
+        {
+            switch (changeEvent.Type)
+            {
+                case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
+
+                    if (!entityDisplays.Any(x => x.ClientId == changeEvent.Value.ClientId))
+                    {
+                        LeaderboardEntityDisplay leaderboardInstance = Instantiate(leaderboardEntityPrefab, leadboardEntityHolder);
+
+                        leaderboardInstance.Initialize(changeEvent.Value.ClientId,
+                                                       changeEvent.Value.PlayerName,
+                                                       changeEvent.Value.Coins);
+
+                        entityDisplays.Add(leaderboardInstance);
+                    }
+
+                    break;
+
+                case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+
+                    LeaderboardEntityDisplay displayToRemove = entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+
+                    if(displayToRemove != null)
+                    {
+                        displayToRemove.transform.SetParent(null);
+                        Destroy(displayToRemove.gameObject);
+                        entityDisplays.Remove(displayToRemove);
+                    }
+
+                    break;
+
+                case NetworkListEvent<LeaderboardEntityState>.EventType.Value:
+
+                    LeaderboardEntityDisplay displayToUpdate = entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+
+                    if(displayToUpdate != null)
+                    {
+                        displayToUpdate.UpdateCoins(changeEvent.Value.Coins);
+                    }
+
+                    break;
+            }
+        }
+
+        private void HandlePlayerSpawned(TankPlayer player)
+        {
+            leaderboardEntities.Add(new LeaderboardEntityState
+            {
+                ClientId = player.OwnerClientId,
+                PlayerName = player.PlayerName.Value,
+                Coins = 0
+            });
+
+        }
+
+        private void HandlePlayerDespawned(TankPlayer player) 
+        {
+            if(leaderboardEntities == null) return;
+
+            foreach(LeaderboardEntityState entity in leaderboardEntities)
+            {
+                if (entity.ClientId != player.OwnerClientId) continue;
+
+                leaderboardEntities.Remove(entity);
+                break;
+            }
+        }
+        
     }
 }
--- Tanks-Netcode/Assets/Scripts/Core/UI/Game/Leaderboard/LeaderboardEntityDisplay.cs
+++ Tanks-Netcode/Assets/Scripts/Core/UI/Game/Leaderboard/LeaderboardEntityDisplay.cs
@@ -1,9 +1,42 @@
 using UnityEngine;
+using TMPro;
+using Unity.Collections;
 
 namespace Tanks
 {
     public class LeaderboardEntityDisplay : MonoBehaviour
     {
-        
+        private TMP_Text displayText;
+
+        public ulong ClientId { get; private set; }
+        public FixedString32Bytes PlayerName { get; private set; }
+        public int Coins { get; private set; }
+
+
+        private void Awake()
+        {
+            displayText = GetComponent<TMP_Text>();
+        }
+
+        public void Initialize(ulong clientId, FixedString32Bytes playerName, int coins)
+        {
+            ClientId = clientId;
+            PlayerName = playerName;
+
+            UpdateCoins(coins);
+        }
+
+        public void UpdateCoins(int coins)
+        {
+            Coins = coins;
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            displayText.text = $"1. {PlayerName} ({Coins})";
+        }
+
+
     }
 }
